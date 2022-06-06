a88c1252857931b7ff8a77892b29fb21e4533582
Eduardo Lobo <eduardo.lobo.alurralde@gmail.com>
2022-06-05 21:46:37 -0300

    typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Quotes App</title>
+        <meta name="Quotes app description" content="Quotes" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>hello</div>
+    </div>
+  );
+}
